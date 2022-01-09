f09889f5eb7e70cec870c5f10a1a050a696f8184
Pakvothe <ortiz.franco@yandex.com>
2022-01-09 17:15:07 -0300

    fixed tests

--- pages/tests/_app.test.tsx
+++ pages/tests/_app.test.tsx
@@ -2,6 +2,22 @@ import { render, screen } from '@testing-library/react';
 import MyApp from '../index';
 import { AppProvider } from '../../context';
 
+const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+
+useRouter.mockImplementation(() => ({
+	route: '/',
+	pathname: '',
+	query: '',
+	asPath: '',
+	push: jest.fn(),
+	events: {
+		on: jest.fn(),
+		off: jest.fn(),
+	},
+	beforePopState: jest.fn(() => null),
+	prefetch: jest.fn(() => null),
+}));
+
 describe('Component <MyApp />', () => {
 	it('The entire application is rendered', () => {
 		render(
--- pages/tests/transactions.test.tsx
+++ pages/tests/transactions.test.tsx
@@ -6,6 +6,22 @@ Object.defineProperty(window, 'fetch', {
 	value: jest.fn(() => Promise.resolve({ json: () => ({}) })),
 });
 
+const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+
+useRouter.mockImplementation(() => ({
+	route: '/',
+	pathname: '',
+	query: '',
+	asPath: '',
+	push: jest.fn(),
+	events: {
+		on: jest.fn(),
+		off: jest.fn(),
+	},
+	beforePopState: jest.fn(() => null),
+	prefetch: jest.fn(() => null),
+}));
+
 describe('Component <Transactions />', () => {
 	it('Renders on screen', () => {
 		render(
